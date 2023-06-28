85d9196fb5796c43b1cd34487d5ff3bfdd64d0e1
MannShah17 <mannshah2004@gmail.com>
2023-06-28 12:44:51 +0530

    done

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,11 +1,9 @@
 const isAuthenticated = (requiredRole) => (req, res, next) => {
   const userRole = req.user.role;
+  //Write your code here for user authentication middleware
 
   // Check if the user's role matches the required role
-  if (userRole !== requiredRole) {
-    return res.status(403).json({ message: 'Forbidden' });
-  }
-
+  //If user role doesnt mathes the requiredRole then it should have status 403 with a Fornidden message
   // User is authenticated and has the required role, proceed to the next middleware
   next();
 };
